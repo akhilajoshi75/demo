3a5eb55f60ebd70642f32bc494f1d531e6fabc0d
akhilajoshi75 <akhilajoshi75@gmail.com>
2024-10-23 21:56:29 +0530

    commiting

---
+++ tt.py
@@ -0,0 +1,20 @@
+if(college==1):
+  6:30 - 8:30   - simpli
+  8:30 - 9:00   - projects(cpp and java)
+  10:00 - 11:00 - devops
+  any other work = do it!
+elif(college==1 && exams==1):
+  6:30 - 9:00 && 9:30 - 11:30 - subject
+  11:30 - 12:30 - simpli // devops
+elif(college==0 && exams==1):
+  10:00 - 1:30 - subject
+  2:30  - 6:00 - simpli && devops
+  7:00  - 9:00 - subject
+  9:30  - 12:00 - subject 
+else:
+  11:00 - 1:00 - simpli
+  2:30  - 4:30 - devops
+  4:30  - 5:30 - projects 
+  7:00  - 9:00 - simpli
+  any other work = do it!   
+ 
